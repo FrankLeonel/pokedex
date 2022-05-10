f4efb7df9623da98d3d04d894188cd2df2243bca
FrankLeonel <frank.leonel@alu.ufc>
2022-05-10 16:17:28 -0300

    retirando a search bar do header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,15 @@
-import { ReactComponent as FilterIcon } from "assets/icons/filter-icon.svg";
 import { ReactComponent as MenuIcon } from "assets/icons/menu-icon.svg";
 import Content from "components/Content";
 import Drawer from "components/Drawer";
 import SelectTheme from "components/SelectTheme";
 import useToggle from "hooks/useToggle";
-import useWindowDimensions from "hooks/useWindowDimension";
 import { Link } from "react-router-dom";
 import * as S from "./Header.style";
 import MenuMain from "./MenuMain";
-import SeachBar from "./SeachBar";
 
 const Header = () => {
-  // const { pokemons } = usePokemons();
-  // const lengthFavorites = pokemons.length;
-
-  const { width } = useWindowDimensions();
-
   const [showMenu, onToggleMenu] = useToggle();
+
   const menuItems = [
     {
       label: "Home",
@@ -45,19 +38,10 @@ const Header = () => {
 
             <S.ContainerOptions>
               <SelectTheme />
-
-              {width >= 768 ? <SeachBar /> : null}
-
-              {width >= 768 ? (
-                <S.FilterButton>
-                  <FilterIcon />
-                </S.FilterButton>
-              ) : null}
             </S.ContainerOptions>
           </S.Section>
         </Content>
       </S.Container>
-      {width < 768 ? <SeachBar /> : null}
       <Drawer show={showMenu} items={menuItems} onClose={onToggleMenu} />
     </S.Header>
   );
